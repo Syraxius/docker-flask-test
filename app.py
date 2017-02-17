bd44794d5247d8d47648c4f23baf3e00673d4da1
Ang Kah Min <syraxius@hotmail.com>
2017-02-17 15:07:11 +0800

    Changed a word

--- app.py
+++ app.py
@@ -2,12 +2,13 @@ from flask import Flask
 from redis import Redis
 
 app = Flask(__name__)
-redis = Redis(host='redis', port=6379)
+#redis = Redis(host='redis', port=6379)
+redis = Redis(host='marathon-lb-internal.marathon.mesos', port=6379)
 
 @app.route('/')
 def hello():
     redis.incr('hits')
-    return 'Hello world! This page has been refreshed %s times' % redis.get('hits')
+    return 'Hello world! This page has been viewed %s times' % redis.get('hits')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0")
